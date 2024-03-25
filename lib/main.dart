99ccb20fec529b257d77bb907f85261ab8683691
Kevin Thompson <phlux@ewnix.net>
2024-03-25 16:56:30 -0500

    Completely rework theming

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,23 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'themes.dart';
 import 'theme_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
-  // Check if there are any saved networks
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure plugin services are initialized
+  final prefs = await SharedPreferences
+      .getInstance(); // Await the SharedPreferences instance
+
   final hasNetworks = await _hasSavedNetworks();
 
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeProvider(
         darkTheme: darkErgonaut,
-        lightTheme: lightErgonaut,
+        lightTheme: lightErgonaut, prefs: prefs, // Use the awaited prefs
       ),
-      child: Ergonaut(hasNetworks: hasNetworks),
+      child: Ergonaut(hasNetworks: hasNetworks, themeMode: ThemeMode.system),
     ),
   );
 }
@@ -24,14 +29,49 @@ Future<bool> _hasSavedNetworks() async {
   // For now, assume no networks are saved
   return false;
 }
+
 enum ThemeOption {
   light,
   dark,
 }
-class Ergonaut extends StatelessWidget {
-  final bool hasNetworks;
 
-  Ergonaut({Key? key, required this.hasNetworks}) : super(key: key);
+class Ergonaut extends StatefulWidget {
+  // Changed to StatefulWidget
+  final bool hasNetworks;
+  final ThemeMode themeMode; // Added
+
+  Ergonaut(
+      {Key? key, required this.hasNetworks, this.themeMode = ThemeMode.dark})
+      : super(key: key); // Updated
+
+  @override
+  _ErgonautState createState() => _ErgonautState();
+}
+
+class _ErgonautState extends State<Ergonaut> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _updateTheme();
+    });
+  }
+
+  void _updateTheme() {
+    final brightness =
+        WidgetsBinding.instance.platformDispatcher.platformBrightness;
+    if (brightness == Brightness.dark) {
+      Provider.of<ThemeProvider>(context, listen: false).setDarkTheme();
+    } else {
+      Provider.of<ThemeProvider>(context, listen: false).setLightTheme();
+    }
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    _updateTheme();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,60 +81,66 @@ class Ergonaut extends StatelessWidget {
       home: Scaffold(
         body: Padding(
           padding: EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  PopupMenuButton<ThemeOption>(
+                    onSelected: (ThemeOption value) {
+                      if (value == ThemeOption.light) {
+                        Provider.of<ThemeProvider>(context, listen: false)
+                            .setLightTheme();
+                      } else if (value == ThemeOption.dark) {
+                        Provider.of<ThemeProvider>(context, listen: false)
+                            .setDarkTheme();
+                      }
+                    },
+                    itemBuilder: (BuildContext context) =>
+                        <PopupMenuEntry<ThemeOption>>[
+                      const PopupMenuItem<ThemeOption>(
+                        value: ThemeOption.light,
+                        child: Text('Light Theme'),
+                      ),
+                      const PopupMenuItem<ThemeOption>(
+                        value: ThemeOption.dark,
+                        child: Text('Dark Theme'),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                ],
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    PopupMenuButton<ThemeOption>(
-                      onSelected: (ThemeOption value) {
-                        if (value == ThemeOption.light) {
-                          Provider.of<ThemeProvider>(context, listen: false).setLightTheme();
-                        } else if (value == ThemeOption.dark) {
-                          Provider.of<ThemeProvider>(context, listen: false).setDarkTheme();
-                        }
-                      },
-                      itemBuilder: (BuildContext context) => <PopupMenuEntry<ThemeOption>>[
-                        const PopupMenuItem<ThemeOption>(
-                          value: ThemeOption.light,
-                          child: Text('Light Theme'),
-                        ),
-                        const PopupMenuItem<ThemeOption>(
-                          value: ThemeOption.dark,
-                          child: Text('Dark Theme'),
-                        ),
-                      ],
-                    ),
-                    Spacer(),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        height: MediaQuery.of(context).size.height * 0.7 / ( 1024 / 1080),
-                        child: SvgPicture.asset(
-                          'assets/logo.svg',
-                          semanticsLabel: 'Ergonaut logo',
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      height: MediaQuery.of(context).size.height *
+                          0.7 /
+                          (1024 / 1080),
+                      child: SvgPicture.asset(
+                        'assets/logo.svg',
+                        semanticsLabel: 'Ergonaut logo',
                       ),
                     ),
                     SizedBox(height: 20),
-                    hasNetworks
-                      ? Text('Your IRC Networks') // Replace with actual UI for networks
-                      : ElevatedButton(
-                        onPressed: () {
-                        // Handle "Add Network" button press
-                        },
-                        child: const Text('Add Network'),
-                   ),
-                 ],
-               ),
-             ),
-           ],
-         ),
-       ),
+                    widget.hasNetworks
+                        ? Text(
+                            'Your IRC Networks') // Replace with actual UI for networks
+                        : ElevatedButton(
+                            onPressed: () {
+                              // Handle "Add Network" button press
+                            },
+                            child: const Text('Add Network'),
+                          ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/theme_provider.dart
+++ lib/theme_provider.dart
@@ -1,14 +1,22 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+import 'package:win32/win32.dart';
+import 'package:ffi/ffi.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  late ThemeData _themeData;
+  ThemeData _themeData;
   late SharedPreferences _prefs;
   final ThemeData darkTheme;
   final ThemeData lightTheme;
 
-  ThemeProvider({required this.darkTheme, required this.lightTheme}) {
-    _themeData = darkTheme;
+  ThemeProvider(
+      {required this.darkTheme,
+      required this.lightTheme,
+      required SharedPreferences prefs})
+      : _prefs = prefs,
+        _themeData = darkTheme {
     _loadTheme();
   }
 
@@ -34,9 +42,41 @@ class ThemeProvider extends ChangeNotifier {
 
   void _loadTheme() async {
     _prefs = await SharedPreferences.getInstance();
-    final isDark = _prefs.getBool('isDark') ?? false;
+    final isDark = _prefs.getBool('isDark') ?? _isSystemDarkTheme();
     _themeData = isDark ? darkTheme : lightTheme;
     notifyListeners();
+  }
+
+  bool _isSystemDarkTheme() {
+    if (Platform.isAndroid || Platform.isIOS) {
+      // Use MediaQuery.of(context).platformBrightness for Android & iOS in main.dart
+      return false; // Placeholder, actual check is done in main.dart
+    } else if (Platform.isWindows) {
+      final hKey = calloc<IntPtr>();
+      const subKey =
+          'Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize';
+      var result = RegOpenKeyEx(HKEY_CURRENT_USER,
+          subKey.toNativeUtf16().cast<Utf16>(), 0, KEY_READ, hKey);
+      if (result == ERROR_SUCCESS) {
+        final pvData = calloc<Uint32>();
+        final lpcbData = calloc<Uint32>()
+          ..value = 4; // Assuming DWORD (4 bytes)
+        result = RegQueryValueEx(hKey.value, TEXT('AppsUseLightTheme'), nullptr,
+            nullptr, pvData as Pointer<Uint8>, lpcbData);
+        if (result == ERROR_SUCCESS) {
+          final isLightTheme = pvData.value != 0;
+          RegCloseKey(hKey.value);
+          calloc.free(hKey);
+          calloc.free(pvData);
+          calloc.free(lpcbData);
+          return !isLightTheme; // Inverts isLightTheme to match _isSystemDarkTheme logic
+        }
+      }
+    } else if (Platform.isLinux || Platform.isMacOS) {
+      // Assuming dark theme by default for Linux and macOS, adjust as needed
+      return true;
+    }
+    return true; // Default to dark theme
   }
 
   void _saveTheme() {
